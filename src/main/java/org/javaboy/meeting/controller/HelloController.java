8f0a1164404c246c33db564f2c8569bb08e732e2
zhaomo <zhaomo0823@gmail.com>
2022-03-31 18:24:06 +0800

    Initial commit

---
+++ src/main/java/org/javaboy/meeting/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.javaboy.meeting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/111")
+    public String hello(){
+        return "hello";
+    }
+}
